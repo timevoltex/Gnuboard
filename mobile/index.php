3291c4f9b890978dc9c20ae97b7dc529521f405b
yeoneum <52909529+timevoltex@users.noreply.github.com>
2020-03-25 17:33:28 +0900

    20.03.25 오타 수정

--- mobile/index.php
+++ mobile/index.php
@@ -111,7 +111,7 @@ include_once(G5_MOBILE_PATH . '/head.php');
     <div class="btn-wrapper">
       <button type="button" class="goDown black">
       <div class="btn-inner">
-        <p class="normal">VALUE</p>
+        <p class="normal">CONTACT</p>
         <i style="font-size:1.5em" class="fa fa-caret-down" aria-hidden="true"></i><span class="sound_only">상단으로</span>
       </div>
     </button>
