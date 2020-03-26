ede454f8ac69bfe2d9bb30b44f30f5c9ca00de5d
yeoneum <52909529+timevoltex@users.noreply.github.com>
2020-03-26 11:06:06 +0900

    20.03.26 jQuery 및 메뉴버튼 변경
    줄 정렬 및 메뉴버튼과 닫기버튼을 수정함
    jQuery수정을 통해 메뉴버튼과 닫기버튼 위치를 동일하게함

--- mobile/head.php
+++ mobile/head.php
@@ -1,54 +1,56 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-if(defined('G5_THEME_PATH')) {
-    require_once(G5_THEME_PATH.'/head.php');
-    return;
+if (defined('G5_THEME_PATH')) {
+  require_once(G5_THEME_PATH . '/head.php');
+  return;
 }
 
-include_once(G5_PATH.'/head.sub.php');
-include_once(G5_LIB_PATH.'/latest.lib.php');
-include_once(G5_LIB_PATH.'/outlogin.lib.php');
-include_once(G5_LIB_PATH.'/poll.lib.php');
-include_once(G5_LIB_PATH.'/visit.lib.php');
-include_once(G5_LIB_PATH.'/connect.lib.php');
-include_once(G5_LIB_PATH.'/popular.lib.php');
+include_once(G5_PATH . '/head.sub.php');
+include_once(G5_LIB_PATH . '/latest.lib.php');
+include_once(G5_LIB_PATH . '/outlogin.lib.php');
+include_once(G5_LIB_PATH . '/poll.lib.php');
+include_once(G5_LIB_PATH . '/visit.lib.php');
+include_once(G5_LIB_PATH . '/connect.lib.php');
+include_once(G5_LIB_PATH . '/popular.lib.php');
 ?>
 
 <header id="hd">
-    <h1 id="hd_h1"><?php echo $g5['title'] ?></h1>
+  <h1 id="hd_h1"><?php echo $g5['title'] ?></h1>
 
-    <div class="to_content"><a href="#container">본문 바로가기</a></div>
+  <div class="to_content"><a href="#container">본문 바로가기</a></div>
 
-    <?php
-    if(defined('_INDEX_')) { // index에서만 실행
-        include G5_MOBILE_PATH.'/newwin.inc.php'; // 팝업레이어
-    } ?>
+  <?php
+  if (defined('_INDEX_')) { // index에서만 실행
+    include G5_MOBILE_PATH . '/newwin.inc.php'; // 팝업레이어
+  } ?>
 
-    <div id="hd_wrapper">
+  <div id="hd_wrapper">
 
-        <div id="logo">
-            <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_IMG_URL ?>/Connple-white.png" alt="<?php echo $config['cf_title']; ?>"></a>
-        </div>
+    <div id="logo">
+      <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_IMG_URL ?>/Connple-white.png" alt="<?php echo $config['cf_title']; ?>"></a>
+    </div>
 
-        <button type="button" id="gnb_open" class="hd_opener"><i class="fa fa-bars" aria-hidden="true"></i><span class="sound_only"> 메뉴열기</span></button>
+    <button type="button" id="gnb_open" class="hd_div_closer" style="color:black;"><i class="fa fa-bars" aria-hidden="true"></i><span class="sound_only"> 메뉴열기</span></button>
+    <button type="button" id="gnb_open" class="hd_opener"><i class="fa fa-bars" aria-hidden="true"></i><span class="sound_only"> 메뉴열기</span></button>
 
-        <div id="gnb" class="hd_div">
-          <div class="nav-wrapper">
-            <ul class="nav">
-              <li><a class="nav-menu active" href="/">회사소개</a></li>
-              <li><a class="nav-menu" href="#">사업영역</a></li>
-              <li><a class="nav-menu" href="#">커넥트 피플</a></li>
-              <li><a class="nav-menu" href="#">고객센터</a></li>
-            </ul>
-            <button type="button" id="gnb_close" class="hd_closer"><span class="sound_only">메뉴 닫기</span><i style="font-size:15px;" class="fa fa-times" aria-hidden="true"></i></button>
-            </div>
-			<?php //echo outlogin('basic'); // 외부 로그인 ?>
-    
-        </div>
+    <div id="gnb" class="hd_div">
+      <div class="nav-wrapper">
+        <ul class="nav">
+          <li><a class="nav-menu active" href="/">회사소개</a></li>
+          <li><a class="nav-menu" href="#">사업영역</a></li>
+          <li><a class="nav-menu" href="#">커넥트 피플</a></li>
+          <li><a class="nav-menu" href="#">고객센터</a></li>
+        </ul>
+        <!-- <button type="button" id="gnb_close" class="hd_closer"><span class="sound_only">메뉴 닫기</span><i style="font-size:15px;" class="fa fa-times" aria-hidden="true"></i></button> -->
+      </div>
+      <?php //echo outlogin('basic'); // 외부 로그인 
+      ?>
 
-        <!-- <button type="button" id="user_btn" class="hd_opener"><i class="fa fa-search" aria-hidden="true"></i><span class="sound_only">사용자메뉴</span></button> -->
-        <!-- <div class="hd_div" id="user_menu">
+    </div>
+
+    <!-- <button type="button" id="user_btn" class="hd_opener"><i class="fa fa-search" aria-hidden="true"></i><span class="sound_only">사용자메뉴</span></button> -->
+    <!-- <div class="hd_div" id="user_menu">
             <button type="button" id="user_close" class="hd_closer"><span class="sound_only">메뉴 닫기</span><i class="fa fa-times" aria-hidden="true"></i></button>
             <div id="hd_sch">
                 <h2>사이트 내 전체검색</h2>
@@ -87,64 +89,61 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
                 }
                 </script>
             </div> -->
-            <?php //echo popular('basic'); // 인기검색어 ?>
-            <!-- <div id="text_size"> -->
-            <!-- font_resize('엘리먼트id', '제거할 class', '추가할 class'); -->
-                <!-- <button id="size_down" onclick="font_resize('container', 'ts_up ts_up2', '', this);" class="select"><img src="<?php echo G5_URL; ?>/img/ts01.png" width="20" alt="기본"></button>
+    <?php //echo popular('basic'); // 인기검색어 
+    ?>
+    <!-- <div id="text_size"> -->
+    <!-- font_resize('엘리먼트id', '제거할 class', '추가할 class'); -->
+    <!-- <button id="size_down" onclick="font_resize('container', 'ts_up ts_up2', '', this);" class="select"><img src="<?php echo G5_URL; ?>/img/ts01.png" width="20" alt="기본"></button>
                 <button id="size_def" onclick="font_resize('container', 'ts_up ts_up2', 'ts_up', this);"><img src="<?php echo G5_URL; ?>/img/ts02.png" width="20" alt="크게"></button>
                 <button id="size_up" onclick="font_resize('container', 'ts_up ts_up2', 'ts_up2', this);"><img src="<?php echo G5_URL; ?>/img/ts03.png" width="20" alt="더크게"></button>
             </div> -->
-        </div>
+  </div>
 
-        <script>
-        $(function () {
-            //폰트 크기 조정 위치 지정
-            var font_resize_class = get_cookie("ck_font_resize_add_class");
-            if( font_resize_class == 'ts_up' ){
-                $("#text_size button").removeClass("select");
-                $("#size_def").addClass("select");
-            } else if (font_resize_class == 'ts_up2') {
-                $("#text_size button").removeClass("select");
-                $("#size_up").addClass("select");
-            }
+  <script>
+    $(function() {
+      //폰트 크기 조정 위치 지정
+      var font_resize_class = get_cookie("ck_font_resize_add_class");
+      if (font_resize_class == 'ts_up') {
+        $("#text_size button").removeClass("select");
+        $("#size_def").addClass("select");
+      } else if (font_resize_class == 'ts_up2') {
+        $("#text_size button").removeClass("select");
+        $("#size_up").addClass("select");
+      }
 
-            $(".hd_opener").on("click", function() {
-                var $this = $(this);
-                var $hd_layer = $this.next(".hd_div");
+      $(".hd_opener").on("click", function() {
+        var $this = $(this);
+        var $hd_layer = $this.next(".hd_div");
 
-                if($hd_layer.is(":visible")) {
-                    $hd_layer.hide();
-                    $this.find("span").text("열기");
-                } else {
-                    var $hd_layer2 = $(".hd_div:visible");
-                    $hd_layer2.prev(".hd_opener").find("span").text("열기");
-                    $hd_layer2.hide();
+        if ($hd_layer.is(":visible")) {
+          $hd_layer.hide();
+          $this.find("span").text("열기");
+        } else {
+          var $hd_layer2 = $(".hd_div:visible");
+          $hd_layer2.prev(".hd_opener").find("span").text("열기");
+          $hd_layer2.hide();
 
-                    $hd_layer.show();
-                    $this.find("span").text("닫기");
-                }
-            });
+          $hd_layer.show();
+          $(".hd_div_closer").show();
+          $this.hide();
+          $this.find("span").text("닫기");
+        }
+      });
 
-            $("#container").on("click", function() {
-                $(".hd_div").hide();
+      $(".section").on("click", function() {
+        $(".hd_div, .hd_div_closer").hide();
+        $(".hd_opener").show();
 
-            });
+      });
 
-            $(".btn_gnb_op").click(function(){
-                $(this).toggleClass("btn_gnb_cl").next(".gnb_2dul").slideToggle(300);
-                
-            });
+      $(".hd_div_closer").on("click", function() {
+        var idx = $(".hd_div_closer").index($(this));
+        $(".hd_div:visible, .hd_div_closer").hide();
+        $(".hd_opener:eq(" + idx + ")").find("span").text("열기");
+        $(".hd_opener:eq(" + idx + ")").show();
+      });
+    });
+  </script>
 
-            $(".hd_closer").on("click", function() {
-                var idx = $(".hd_closer").index($(this));
-                $(".hd_div:visible").hide();
-                $(".hd_opener:eq("+idx+")").find("span").text("열기");
-            });
-        });
-        </script>
-        
-    </div>
+  </div>
 </header>
-
-
-
