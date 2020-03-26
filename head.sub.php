b29cf0d1e39a434a1146906f7fe223c08dd7c6a4
yeoneum <52909529+timevoltex@users.noreply.github.com>
2020-03-26 15:32:58 +0900

    meta 데이터 추가

--- head.sub.php
+++ head.sub.php
@@ -50,8 +50,15 @@ if (G5_IS_MOBILE) {
     echo '<meta http-equiv="imagetoolbar" content="no">'.PHP_EOL;
     echo '<meta http-equiv="X-UA-Compatible" content="IE=Edge">'.PHP_EOL;
 }
+echo '<link rel="canonical href="http://www.connple.com">'.PHP_EOL;
+echo '<meta name="robots" content="index, nofollow">'.PHP_EOL;
+echo '<meta name="description" content="사람과 사람을 연결하는 기업, 컨플입니다.">'.PHP_EOL;
+echo '<meta property="og:type" content="website">'.PHP_EOL;
+echo '<meta property="og:title" content="CONNPLE">'.PHP_EOL;
+echo '<meta property="og:description" content="사람과 사람을 연결하는 기업, 컨플입니다.">'.PHP_EOL;
+echo '<meta property="og:imgage" content="http://www.connple.com/img/connple logo.png">'.PHP_EOL;
+echo '<meta property="og:url" content="http://www.connple.com">'.PHP_EOL;
 
-if($config['cf_add_meta'])
     echo $config['cf_add_meta'].PHP_EOL;
 ?>
 <title>CONNPLE</title>
