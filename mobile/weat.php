d6a63e60d1aab329719e030268482096c5fc20fc
yeoneum <52909529+timevoltex@users.noreply.github.com>
2020-03-31 17:25:02 +0900

    20.03.31 사업소개 페이지 추가

---
+++ mobile/weat.php
@@ -0,0 +1,3 @@
+<?php
+  include_once('./_head.php');
+?>
---
+++ weat.php
@@ -0,0 +1,8 @@
+<?php 
+  include_once('/_head.php');
+  if(G5_IS_MOBILE){
+    include_once(G5_MOBILE_PATH.'/weat.php');
+    return;
+  }
+?>
+<div id=""></div>
